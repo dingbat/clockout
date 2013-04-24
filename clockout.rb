758f3b1111a648400e190df21dee206a24922785
Dan Hassin <danhassin@mac.com>
2013-04-24 14:03:11 -0400

    Add Commit class to keep track of minutes per commit

--- clockout.rb
+++ clockout.rb
@@ -5,23 +5,33 @@ repo = Grit::Repo.new(ARGV[0])
 commits = repo.commits('master', 500)
 commits.reverse!
 
+class Commit
+	attr_accessor :message, :minutes, :date
+end
+
 def seperate_into_blocks(repo, commits)
 	blocks = []
 	block = []
 
 	commits.each do |commit|
-		date = commit.committed_date
+
+		c = Commit.new
+		c.date = commit.committed_date
+		c.message = commit.message
+		c.minutes = 0
 
 		if block.size > 0
-			time_since_last = (block.last.committed_date - date).abs/60
-			#if it's been 2 hrs since last commit, new block
-			if (time_since_last > 120)
+			time_since_last = (block.last.date - commit.committed_date).abs/60
+			
+			if (time_since_last > 120) #new block cutoff at 2 hrs
 				blocks << block
 				block = []
+			else
+				c.minutes = time_since_last
 			end
 		end
 
-		block << commit
+		block << c
 	end
 
 	blocks << block
@@ -30,7 +40,7 @@ end
 blocks = seperate_into_blocks(repo, commits)
 blocks.each do |block|
 	block.each do |commit|
-		puts commit.message
+		puts "#{commit.minutes.round(2)} #{commit.message}"
 	end
 	puts
 end
