34f1d7ae8116c108190f600bf3ab619ec50f4103
Dan Hassin <danhassin@mac.com>
2013-04-24 14:09:51 -0400

    Add time estimation to commits at start of blocks, based on size of diff

--- clockout.rb
+++ clockout.rb
@@ -9,6 +9,24 @@ class Commit
 	attr_accessor :message, :minutes, :date
 end
 
+def estimate_commit_minutes(commit)
+	plus, minus = 0, 0
+
+	commit.stats.to_diffstat.each do |diff_stat|
+		# make sure the change was with code (adding, say, an image will give tons of additions)
+		code = (diff_stat.filename =~ /\.(m|h|txt)$/)
+		# I didn't write this file, ignore it
+		lx = (diff_stat.filename =~ /LXReorderableCollectionViewFlowLayout/)
+		if code && !lx
+			plus += diff_stat.additions
+			minus += diff_stat.deletions
+		end
+	end
+
+	#estimating (through trial and error) 4 additions and 8 deletions per minute
+	return (plus/4+minus/8)
+end
+
 def seperate_into_blocks(repo, commits)
 	blocks = []
 	block = []
@@ -26,6 +44,9 @@ def seperate_into_blocks(repo, commits)
 			if (time_since_last > 120) #new block cutoff at 2 hrs
 				blocks << block
 				block = []
+
+				#first commit of the block, so we don't know when work started
+				c.minutes = estimate_commit_minutes(commit)
 			else
 				c.minutes = time_since_last
 			end
